559a3da99a7e3e457348c0ef7f4e240f41461785
bohdanmuko <bohdan.mukomela@oa.edu.ua>
2025-06-08 11:03:46 +0000

    Add post creation, deletion and category parent display

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -10,6 +10,7 @@ class BlogCategory extends Model
 {
     use SoftDeletes;
     use HasFactory;
+    const ROOT = 1;
     protected $fillable
         = [
             'title',
@@ -17,4 +18,40 @@ class BlogCategory extends Model
             'parent_id',
             'description',
         ];
+
+    /**
+     * Батьківська категорія
+     *
+     * @return BlogCategory
+     */
+    public function parentCategory()
+    {
+        //належить категорії
+        return $this->belongsTo(BlogCategory::class, 'parent_id', 'id');
+    }
+
+    /**
+     * Приклад аксесуара (Accessor)
+     *
+     * @return string
+     */
+    public function getParentTitleAttribute()
+    {
+        $title = $this->parentCategory->title
+            ?? ($this->isRoot()
+                ? 'Корінь'
+                : '???');
+
+        return $title;
+    }
+
+    /**
+     * Перевірка чи об'єкт є кореневим
+     *
+     * @return bool
+     */
+    public function isRoot()
+    {
+        return $this->id === BlogCategory::ROOT;
+    }
 }
--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 class BlogPost extends Model
 {
     use HasFactory, SoftDeletes;
+    const UNKNOWN_USER = 1;
     protected $fillable = [
         'title',
         'slug',
@@ -17,7 +18,6 @@ class BlogPost extends Model
         'content_raw',
         'is_published',
         'published_at',
-        'user_id',
     ];
 
     /**
